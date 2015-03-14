7cf8465de5a3ba99c113fb94946612f53b1fa874
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-03-14 13:22:39 -0500

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name campaignWebsiteApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the campaignWebsiteApp
+ */
+angular.module('campaignWebsiteApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name campaignWebsiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the campaignWebsiteApp
+ */
+angular.module('campaignWebsiteApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
