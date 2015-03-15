2fb6e663a19c716995f25b3f2aca5839948fbc3b
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2015-03-15 14:55:05 -0500

    masonry on endorsements page complete with random order

--- app/scripts/controllers/endorsements.js
+++ app/scripts/controllers/endorsements.js
@@ -1,55 +1,85 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name campaignWebsiteApp.controller:EndorsementsCtrl
- * @description
- * # EndorsementsCtrl
- * Controller of the campaignWebsiteApp
- */
+
 angular.module('campaignWebsiteApp')
 	.controller('EndorsementsCtrl', function ($rootScope, $scope, $route) {
 
-  	$rootScope.activeTab = $route.current.activeTab;
-
-  	var $container = $('.js-masonry');
-	// initialize
-	$('.js-masonry').imagesLoaded(function() {
-		$container.masonry({
-	  		gutter: 20,
-	  		itemSelector: '.block',
-	  		isAnimated: true,
-		});
-	});
-
-	// var msnry = $container.data('masonry');
+        // Allows nav panel to highlight active tab
+      	$rootScope.activeTab = $route.current.activeTab;
 
 
-    $scope.blocks = [
-    	{
-    		className: 'endorsement',
-    		name: 'Patrick Franks',
-    		position: 'President, Robotics Club',
-    		image: 'https://unsplash.it/500/500?image=646',
-    		rowCol: [0, 1],
-    		size: [1, 1]
+        // Set up Masonry
+      	var $container = $('ul.js-masonry');
+    	$('ul.js-masonry').imagesLoaded(function() {
+    		$container.masonry({
+                columnWidth: 260,
+                itemSelector: '.block',
+    	  		gutter: 20,
+    	  		isAnimated: true,
+    		});
+    	});
 
-    	},
-    	{
-    		className: 'endorsement',
-    		name: 'Jon Rovira',
-    		position: 'Ex-CEO, AirHop',
-    		image: 'https://unsplash.it/500/500?image=550',
-    		rowCol: [0, 2],
-    		size: [1, 1]
-    	},
-    	{
-    		className: 'endorsement',
-    		name: 'Cody Schiffer',
-    		position: 'President, Bitch Club',
-    		image: 'https://unsplash.it/500/500?image=633',
-    		rowCol: [0, 3],
-    		size: [1, 1]
-    	}
-    ]
+        // Endorsement list
+        $scope.blocks = [
+        	{
+        		className: 'endorsement',
+        		name: 'Patrick Franks',
+        		position: 'President, Robotics Club',
+        		image: 'https://unsplash.it/500/500?image=646'
+        	},
+            {
+                className: 'highlight',
+                text: "Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you."
+            },
+        	{
+        		className: 'endorsement',
+        		name: 'Jon Rovira',
+        		position: 'Ex-CEO, AirHop',
+        		image: 'https://unsplash.it/500/500?image=550'
+        	},
+        	{
+        		className: 'endorsement',
+        		name: 'Cody Schiffer',
+        		position: 'President, Bitch Club',
+        		image: 'https://unsplash.it/500/500?image=633'
+        	},
+            {
+                className: 'endorsement',
+                name: 'Chris Dayal',
+                position: 'President, Carib Nation',
+                image: 'https://unsplash.it/500/500?image=634'
+            },
+            {
+                className: 'endorsement',
+                name: 'Bryan Berger',
+                position: 'President, Points for a Purpose',
+                image: 'https://unsplash.it/500/500?image=635'
+            },
+            {
+                className: 'endorsement',
+                name: 'Chris Howard',
+                position: 'Vice President, ODB',
+                image: 'https://unsplash.it/500/500?image=636'
+            },
+            {
+                className: 'highlight',
+                text: "Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. "
+            },
+            {
+                className: 'endorsement',
+                name: 'Christina Kim',
+                position: 'Vice President, ASG',
+                image: 'https://unsplash.it/500/500?image=637'
+            },
+            {
+                className: 'endorsement',
+                name: 'Noah Starr',
+                position: 'President, ASG',
+                image: 'https://unsplash.it/500/500?image=638'
+            },
+        ];
+
+        $scope.random = function() {
+            return 0.5 - Math.random();
+        }
   });
