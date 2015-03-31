74a2ad1c0b56f52a9d7beaa8badd7f9ddb6b44a2
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-03-31 04:22:50 -0500

    replacing pictures with S3 served ones and commenting out most of app.js (routing)

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,41 +19,46 @@ angular
   ])
   .config(function ($routeProvider) {
     $routeProvider
+      // .when('/', {
+      //   templateUrl: 'views/partials/main.html',
+      //   controller: 'MainCtrl',
+      //   activeTab: 'main'
+      // })
       .when('/', {
         templateUrl: 'views/partials/splash.html',
         controller: 'SplashCtrl',
         activeTab: 'splash'
       })
-      .when('/about', {
-        templateUrl: 'views/partials/about.html',
-        controller: 'AboutCtrl',
-        activeTab: 'about'
-      })
-      .when('/vision', {
-        templateUrl: 'views/partials/vision.html',
-        controller: 'VisionCtrl',
-        activeTab: 'vision'
-      })
-      .when('/endorsements', {
-        templateUrl: 'views/partials/endorsements.html',
-        controller: 'EndorsementsCtrl',
-        activeTab: 'endorsements'
-      })
-      .when('/team', {
-        templateUrl: 'views/partials/team.html',
-        controller: 'TeamCtrl',
-        activeTab: 'team'
-      })
-      .when('/media-gallery', {
-        templateUrl: 'views/partials/media-gallery.html',
-        controller: 'MediaGalleryCtrl',
-        activeTab: 'listening'
-      })
-      .when('/splash', {
-        templateUrl: 'views/partials/splash.html',
-        controller: 'SplashCtrl',
-        activeTab: 'splash'
-      })
+      // .when('/about', {
+      //   templateUrl: 'views/partials/about.html',
+      //   controller: 'AboutCtrl',
+      //   activeTab: 'about'
+      // })
+      // .when('/vision', {
+      //   templateUrl: 'views/partials/vision.html',
+      //   controller: 'VisionCtrl',
+      //   activeTab: 'vision'
+      // })
+      // .when('/endorsements', {
+      //   templateUrl: 'views/partials/endorsements.html',
+      //   controller: 'EndorsementsCtrl',
+      //   activeTab: 'endorsements'
+      // })
+      // .when('/team', {
+      //   templateUrl: 'views/partials/team.html',
+      //   controller: 'TeamCtrl',
+      //   activeTab: 'team'
+      // })
+      // .when('/media-gallery', {
+      //   templateUrl: 'views/partials/media-gallery.html',
+      //   controller: 'MediaGalleryCtrl',
+      //   activeTab: 'listening'
+      // })
+      // .when('/splash', {
+      //   templateUrl: 'views/partials/splash.html',
+      //   controller: 'SplashCtrl',
+      //   activeTab: 'splash'
+      // })
       .otherwise({
         redirectTo: '/'
       });
