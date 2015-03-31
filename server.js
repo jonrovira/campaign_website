e66ef654f6b1bc7e1a89cd399e2d91db5735756d
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-03-31 04:41:26 -0500

    pushing build to github

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 
 var express = require('express'),
-	path = require('path'),
 	app = express();
 
-app.use(express.static(path.join(__dirname + '/dist')));
+app.use(express.static(__dirname + '/dist'));
 app.get('/', function(req, res) {
-	res.sendfile('index.html');
+	res.sendfile('index.html', {root: __dirname + '/dist'});
 });
 
 app.listen(process.env.PORT || 5000);
